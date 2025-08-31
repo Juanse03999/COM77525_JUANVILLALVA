98b8fef36c09942449464faa805d76fc780e720a
Juan Pablo Villalva <juanpablovillalva2@gmail.com>
2025-08-31 03:45:46 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-fsdfsdaf
+import http from 'http';
+
+const port = 8080; 
+const server = http.createServer(
+
+    // Dentro trabajar con objetos manejados por callbacks
+    (request, response) =>{
+        response.end ("Mi segundo server activo")
+    }
+
+
+)
+
+server.listen(port , ()=>{
+    console.log(`escuchando puerto: ${port}`);
+})
